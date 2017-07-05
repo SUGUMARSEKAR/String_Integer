a31a9dd653185be0ae75c22902e4e5d9b476af17
SUGUMAR SEKAR <majesticsugu@gmail.com>
2017-07-05 10:39:25 +0530

    Update Rev1.java

--- Rev1.java
+++ Rev1.java
@@ -1,12 +1,12 @@
 import java.io.*;
 import java.util.*;
-public class Rev1
+public class Fact
 {
-public class void main(String args[])throws IOException
+public static void main(String args[])throws IOException
 {
 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 String x = "300";
 Integer n = Integer.parseInt(x);
-System.out.print((x+100));
+System.out.print((n+100));
 }
 }
