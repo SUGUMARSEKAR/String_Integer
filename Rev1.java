3bfc070a02a4c1769cec387fe873421ba76bcdde
SUGUMAR SEKAR <majesticsugu@gmail.com>
2017-07-05 10:36:18 +0530

    Create Rev1.java

---
+++ Rev1.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Rev1
+{
+public class void main(String args[])throws IOException
+{
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+String x = "300";
+Integer n = Integer.parseInt(x);
+System.out.print((x+100));
+}
+}
